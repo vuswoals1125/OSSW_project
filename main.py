b1e414f884a268e3c1e9dcdbc3ab692faa49c20b
LAPTOP-RJKQOH8P\Park <willyjjj@naver.com>
2024-12-14 17:36:15 +0900

    박상우 4번째과제

--- main.py
+++ main.py
@@ -11,66 +11,14 @@ import time
 #==========[Initialize]==========
 #==========[sensors]==========
 ev3 = EV3Brick()
-gyro = GyroSensor(Port.S1)
 ser = UARTDevice(Port.S2, baudrate=115200)
 
 #==========[motors]==========
-<<<<<<< HEAD
-grab_motor = Motor(Port.A)
-shooting_motor = Motor(Port.B)
-
-left_motor = Motor(Port.C)
-<<<<<<< HEAD
-=======
-gyro = GyroSensor(Port.S4)
-ser = UARTDevice(Port.S3, baudrate=115200)
-ts = TouchSensor(Port.S1)
-cs = ColorSensor(Port.S2)
-
-#==========[motors]==========
-=======
->>>>>>> ba4ee425b3de4330d1c524e87ded11dd67289e96
-grab_motor = Motor(Port.B)
-shooting_motor = Motor(Port.C)
 
 left_motor = Motor(Port.A)
-=======
->>>>>>> pjm3_branch
 right_motor = Motor(Port.D)
 robot = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=115)
 
-#==========[target_angle turn(gyro)]==========
-def turn(target_angle, power):
-<<<<<<< HEAD
-    
-    # left_motor.run(power)
-    # right_motor.run(-power)
-    # while True:
-    #     angle=gyro.angle()
-        
-    #     if abs(angle)>target_angle-2:
-    #         left_motor.stop()
-    #         right_motor.stop()
-    #         break
-    # robot.turn()
-    print('robot turn')
-    robot.drive(power, power)
-    while True:
-        angle = gyro.angle()
-        print(angle)
-        if abs(angle)>target_angle-2:
-=======
-
-    robot.turn(target_angle)  # 이 부분이 동작하지 않음 (매개변수 없음)
-    print('robot turn')
-    robot.drive(power, power)
-    while True: 
-        angle = gyro.angle()
-        print(angle)
-        if abs(angle) > target_angle - 2:
->>>>>>> pjm3_branch
-            robot.stop()
-            break
 
 #==========[camera_chase]==========
 def process_uart_data(data):
@@ -97,166 +45,24 @@ def pd_control(cam_data, kp, kd, power):
     output = (kp * error) + (kd * derivative)
     robot.drive(power, output)
     previous_error = error
- 
-#==========[shooting positions]==========
-def grab(command):
-    if command == 'motion3':
-        #close
-        grab_motor.run_until_stalled(150,Stop.COAST,duty_limit=50)
-        #set_zero point
-        grab_motor.reset_angle(0)
-    elif command == 'motion1':
-        #open1
-        grab_motor.run_until_stalled(-150,Stop.COAST,duty_limit=50)
-    elif command == 'motion2':
-        #open2
-        grab_motor.run_target(150,-150)
-
-def shoot(command):
-    if command == 'zero':
-        #zero_position
-        shooting_motor.run_until_stalled(-180,Stop.COAST,duty_limit=50)
-    elif command == 'shoot':
-        #shooting
-        shooting_motor.run(2000)
-        time.sleep(0.25)
-        shooting_motor.stop()
-
 
 
 #==========[setup]==========
 ev3.speaker.beep()
-threshold = 70
+threshold = 80
 previous_error = 0
-gyro.reset_angle(0)
-#==========[zero set position setting]==========
-shoot('zero') #shoot 모터가 안쪽이고,
-grab('motion3') #grab 모터가 바깥쪽이므로 shoot먼저 세팅 후 grab을 세팅해야한다
-time.sleep(1)
-grab('motion1') #공을 잡기 위한 높이로 열기
 
-print("Zero set postion completed")
 
-#==========[main loop]==========
-while True:
-    data = ser.read_all()
-    # 데이터 처리 및 결과 필터링
-    if ts.pressed():
-        robot.straight(-100)
-    if cs.color() == Color.GREEN:
-        robot.straight(-100)
-    try:
-        filter_result = process_uart_data(data)
-        #filter_result[0] : x, filter_result[1] : y
-        if filter_result[0]!= -1 and filter_result[1]!= -1:
-        # if filter_result[0]!= -1 and filter_result[1]!= -1:
-<<<<<<< HEAD
-            if filter_result[1] > 90: #공이 카메라 화면 기준으로 아래에 위치 = 로봇에 가까워졌다
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-                robot.straight(110) #강제로 앞으로 이동
-=======
-                robot.straight(115) #강제로 앞으로 이동
->>>>>>> pjm3_branch
-                grab('motion3') #공을 잡기
-                time.sleep(1) #동작간 딜레이
-                # turn(0,100) #정면(상대방 진영)바라보기
-                time.sleep(1) #동작간 딜레이
-                grab('motion1') #슛을 위한 열기
-                time.sleep(0.5) #동작간 딜레이d
-<<<<<<< HEAD
-=======
-                robot.straight(150) #강제로 앞으로 이동
-=======
-            if filter_result[1] > 110: #공이 카메라 화면 기준으로 아래에 위치 = 로봇에 가까워졌다
-                robot.straight(120) #강제로 앞으로 이동
->>>>>>> mhj3_branch
-=======
-                robot.straight(100) #강제로 앞으로 이동
->>>>>>> ba4ee425b3de4330d1c524e87ded11dd67289e96
-                grab('motion3') #공을 잡기
-                time.sleep(2) #동작간 딜레이
-                #turn(0,100) #정면(상대방 진영)바라보기
-                time.sleep(0) #동작간 딜레이
-                grab('motion1') #슛을 위한 열기
-<<<<<<< HEAD
-                time.sleep(0.5) #동작간 딜레이
-<<<<<<< HEAD
->>>>>>> c9c8da70c6235912e206a4127af12b20a40b9520
-=======
-                time.sleep(0) #동작간 딜레이
->>>>>>> mhj3_branch
-=======
->>>>>>> ba4ee425b3de4330d1c524e87ded11dd67289e96
-=======
->>>>>>> pjm3_branch
-                shoot('shoot') #공 날리기
-                time.sleep(0) #동작간 딜레이
-                shoot('zero')
-                grab('motion2') 
-            else: #공이 카메라 화면 기준 멀리 위치해 있으면 chase한다
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-                pd_control(filter_result[0], kp=0.7, kd=0.1, power=100)
-=======
-                pd_control(filter_result[0], kp=0.5, kd=0.1, power=100)
->>>>>>> c9c8da70c6235912e206a4127af12b20a40b9520
-=======
-                pd_control(filter_result[0], kp=0.7, kd=0.2, power=150)
->>>>>>> mhj3_branch
-=======
-                pd_control(filter_result[0], kp=0.5, kd=0.1, power=100)
->>>>>>> ba4ee425b3de4330d1c524e87ded11dd67289e96
-=======
-                pd_control(filter_result[0], kp=0.7, kd=0.1, power=100)
->>>>>>> pjm3_branch
-        # else: # 센서가 공을 보지 못했을 경우의 움직임.
-        #     robot.straight(50)
-        #     robot.turn(10)
-
-        time.sleep_ms(30)
-    except:
-        pass
-
+# #==========[main loop]==========
 while True:
     try:
         data = ser.read_all()
         filter_result = process_uart_data(data)
         if filter_result[0]!= -1 and filter_result[1]!= -1:
             print(filter_result)
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> pjm3_branch
-            pd_control(filter_result[0], kp=0.7, kd=0.1, power=100)
-=======
             pd_control(filter_result[0], kp=0.5, kd=0.1, power=100)
->>>>>>> ba4ee425b3de4330d1c524e87ded11dd67289e96
         wait(10)
     except:
         pass
         
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-            pd_control(filter_result[0], kp=0.5, kd=0.1, power=100)
-        wait(10)
-    except:
-        pass
->>>>>>> c9c8da70c6235912e206a4127af12b20a40b9520
-=======
-            pd_control(filter_result[0], kp=0.6, kd=0.2, power=150)
-        wait(10)
-    except:
-        pass 
->>>>>>> mhj3_branch
-=======
->>>>>>> ba4ee425b3de4330d1c524e87ded11dd67289e96
-=======
->>>>>>> pjm3_branch
